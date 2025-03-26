25fca41dea2bce82f8958026f75962c1627c3c6d
anjulmohd <anjulmohammed@gmail.com>
2025-03-26 23:47:33 +0530

    ADDED SECRET KEY CONFIDENTIALTY

--- shop/settings.py
+++ shop/settings.py
@@ -23,8 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#58wb-_lmspfz_55u&k5l2t6ccv=9+1z+75rg42lcq!i2tc#fj'
 
+SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
