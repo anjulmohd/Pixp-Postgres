5da1ebad45daf56f93a9ea08fec2f5ffa48cfaef
anjulmohd <anjulmohammed@gmail.com>
2025-03-26 23:59:01 +0530

    modifies allowed host to 512

--- shop/settings.py
+++ shop/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['pixp.up.railway.app','https://pixp.up.railway.app','127.0.0.1']
+ALLOWED_HOSTS = ['51.21.192.2','pixp.up.railway.app','https://pixp.up.railway.app','127.0.0.1']
 CSRF_TRUSTED_ORIGINS = ['https://pixp.up.railway.app']
 
 
